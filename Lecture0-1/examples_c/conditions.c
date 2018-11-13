bc7dd66b4ce909f53d8c4e966962c0ebf3f583df
Sandor <bakos.webd@gmail.com>
2018-11-13 17:27:50 +0100

    0-1 part1

---
+++ Lecture0-1/examples_c/conditions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int counter = 1;
+int main(void)
+{
+    if (counter > 0) 
+    {
+        printf("counter is more than zero");
+    } else if (counter == 0) {
+        printf("counter is zero");
+    } else {
+        printf("counter is more than zero");
+    }
+}
---
+++ Lecture0-1/examples_c/hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //including the library
+
+int main(void) //you need to use this function when your program needs no initial parameters to run/ load (parameters received from the OS - out of the program it self).
+{
+    printf("Hello World");
+}
